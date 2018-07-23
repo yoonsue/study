d89a763d4494e97a2f587cf267f8219a43cb07de
yoonsue <suejoe1996@gmail.com>
2018-07-23 10:14:53 +0900

    git: how to merge the pull request

---
+++ git/merge.sh
@@ -0,0 +1,11 @@
+
+# pull branch to merge
+git pull https://github.com/chanyoung/labchat.git (branch-name)
+
+# check git status
+git log --oneline --decorate --graph
+
+# if there is a conflict, fix it.
+
+# push the code as master
+git push origin master
